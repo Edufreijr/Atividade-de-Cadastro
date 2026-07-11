96180ac8ee72c3acd3ee0491e5c655c03cbcf54e
Eduardo Freitas Jr <eduardo.morisco@gmail.com>
2026-07-10 21:31:06 -0300

    Teste Cadastro

---
+++ script.js
@@ -0,0 +1,19 @@
+const formulario = document.getElementById("formCadastro");
+
+formulario.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const nome = document.getElementById("nome").value;
+    const email = document.getElementById("email").value;
+    const senha = document.getElementById("senha").value;
+    const dataNascimento = document.getElementById("dataNascimento").value;
+    const interesse = document.getElementById("interesse").value;
+    const mensagem = document.getElementById("mensagem").value;
+
+    console.log("Nome:", nome);
+    console.log("Email:", email);
+    console.log("Senha:", senha);
+    console.log("Data de nascimento:", dataNascimento);
+    console.log("Área de interesse:", interesse);
+    console.log("Mensagem:", mensagem);
+});
